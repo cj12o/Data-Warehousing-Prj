320918422c45b362360455f71cfe69974407f82a
cj12o <cj2004jee@gmail.com>
2025-07-24 00:31:46 +0530

    Update init_layers.sql
    Improved Query,
    This query  recreates schema, whether schema exists or not

--- Sql_Scripts/init_layers.sql
+++ Sql_Scripts/init_layers.sql
@@ -1,5 +1,48 @@
+/*Warning => This Script basicvally drops desired Schema if present, then recreates it.
+Aim is to avoid any kind of issues with Schema_name
+ONLY RUN this script when you want to create brand new Schemas and Drop the existing ones
+*/
+
+
+
+
 --Schema creation
-CREATE SCHEMA IF NOT EXISTS bronze;
-CREATE SCHEMA IF NOT EXISTS silver;
-CREATE SCHEMA IF NOT EXISTS gold;
- 
+DO $$
+BEGIN
+--gold creation
+IF EXISTS(
+	SELECT *
+	FROM INFORMATION_SCHEMA.schemata
+	WHERE INFORMATION_SCHEMA.schemata.schema_name='gold'
+	)THEN 
+		DROP SCHEMA gold;
+		RAISE NOTICE 'Schema gold dropped';
+END IF;
+CREATE SCHEMA gold;
+RAISE NOTICE 'Schema gold created';
+--Silver creation
+IF EXISTS(
+	SELECT 1
+	FROM INFORMATION_SCHEMA.schemata
+	WHERE INFORMATION_SCHEMA.schemata.schema_name='silver'
+	)THEN 
+		DROP SCHEMA silver;
+		RAISE NOTICE 'Schema silver dropped';
+END IF;
+CREATE SCHEMA silver;
+RAISE NOTICE 'Schema silver created';
+--Bronze creation
+IF EXISTS(
+	SELECT 1
+	FROM INFORMATION_SCHEMA.schemata
+	WHERE INFORMATION_SCHEMA.schemata.schema_name='bronze'
+	)THEN 
+		DROP SCHEMA bronze;
+		RAISE NOTICE 'Schema bronze dropped';
+END IF;
+CREATE SCHEMA bronze;
+RAISE NOTICE 'Schema bronze created';
+END
+$$
+
+
